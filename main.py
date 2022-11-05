49b54455a2beeb12697dd05e9f84f5d751217e02
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-11-04 22:40:23 -0300

    Create vídeo put resource

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ app = Flask(__name__)
 api = Api(app)
 
 
+videos = {}
+
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("likes", type=int, help="Likes of the video")
+video_put_args.add_argument("name", type=str, help="Name of the video")
+video_put_args.add_argument("views", type=int, help="Views of the video")
+
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id], 200
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+
+
 class HelloWord(Resource):
     def get(self, name):
         return {
@@ -15,6 +33,7 @@ class HelloWord(Resource):
 
 
 api.add_resource(HelloWord, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -4,3 +4,10 @@ BASE_URL = "http://127.0.0.1:5000/"
 
 response = requests.get(f"{BASE_URL}/helloworld/daniel")
 print(response.json())
+
+response = requests.put(f"{BASE_URL}/video/1", {
+    "views": 10000,
+    "name": "Teste",
+    "likes": 100
+})
+print(response.json())
